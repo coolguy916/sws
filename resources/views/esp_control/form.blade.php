279c636f5184ee2f08597ba7eed2571b2d9eaa28
karma <89170731+coolguy916@users.noreply.github.com>
2023-10-20 21:55:06 +0700

    first commit

---
+++ resources/views/esp_control/form.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.layMain')
+@section('content')
+    <div class="page-wrapper">
+        <!-- ============================================================== -->
+        <!-- Bread crumb and right sidebar toggle -->
+        <!-- ============================================================== -->
+        <div class="page-breadcrumb">
+            <div class="row">
+                <div class="col-12 d-flex no-block align-items-center">
+                    <h4 class="page-title">Form Basic</h4>
+                    <div class="ms-auto text-end">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    Library
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Bread crumb and right sidebar toggle -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Container fluid  -->
+        <!-- ============================================================== -->
+        <div class="container-fluid">
+            <!-- Start Page Content -->
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <div class="card">
+                        <form class="form-horizontal" method="POST" action="#" enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-body">
+                                <h4 class="card-title">Laporan Info</h4>
+                                <div class="form-group row">
+                                    <label for="text"
+                                        class="col-sm-3 text-end control-label col-form-label">Text</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="text" name="text"
+                                            placeholder="Teks" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="number"
+                                        class="col-sm-3 text-end control-label col-form-label">number</label>
+                                    <div class="col-sm-9">
+                                        <input type="number" class="form-control" id="number" name="number"
+                                            placeholder="Number" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="time"
+                                        class="col-sm-3 text-end control-label col-form-label">Time</label>
+                                    <div class="col-sm-9">
+                                        <input type="time" class="form-control" id="time" name="time" required
+                                            style="padding: 10px; border: 2px solid #ccc;">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="gambar"
+                                        class="col-sm-3 text-end control-label col-form-label">Gambar</label>
+                                    <div class="col-sm-9">
+                                        <input type="file" class="form-control @error('gambar') is-invalid @enderror"
+                                            name="gambar" required>
+                                        {{-- @error('gambar')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror --}}
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="content" class="col-sm-3 text-end control-label col-form-label">Text
+                                        Area</label>
+                                    <div class="col-sm-9">
+                                        <textarea class="form-control @error('content') is-invalid @enderror" name="content" rows="5"
+                                            placeholder="Text Area" required>{{ old('content') }}</textarea>
+                                        {{-- @error('content')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror --}}
+                                    </div>
+                                </div>
+
+                                {{-- <input type="hidden" name="id_user" value="{{ auth()->user()->id }}"> --}}
+
+                            </div>
+                            <div class="border-top">
+                                <div class="card-body">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- editor -->
+        <!-- ============================================================== -->
+        <!-- End PAge Content -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right sidebar -->
+        <!-- ============================================================== -->
+        <!-- .right-sidebar -->
+        <!-- ============================================================== -->
+        <!-- End Right sidebar -->
+        <!-- ============================================================== -->
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\espController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(espController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/form', 'form')->name('form');
+
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
