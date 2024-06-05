1f9ddde39501e303da3b75db895b6644b4bad844
MFajarTrimulyo <90810685+MFajarTrimulyo@users.noreply.github.com>
2024-06-05 13:22:48 +0700

    rapiin template

--- resources/views/template2/User/modal-scedule.blade.php
+++ resources/views/template2/User/modal-scedule.blade.php
@@ -1,5 +1,3 @@
-
-
 <!-- Modal -->
 <div class="modal fade" id="modal-scedule" tabindex="-1" role="dialog" aria-labelledby="modal-scedule" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
