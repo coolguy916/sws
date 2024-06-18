e711408fe4104e9b0e1a4d9e64fec5ad68faf77b
karma <89170731+coolguy916@users.noreply.github.com>
2024-06-18 22:56:54 +0700

    scroll bar hilang

--- resources/views/template2/User/esp-controller.blade.php
+++ resources/views/template2/User/esp-controller.blade.php
@@ -1,4 +1,21 @@
-<div class="overflow-scroll" style="height: 34vh;">
-    <div class="row d-flex" id="modules-container">
+<style>
+    .no-scrollbar {
+        height: 16vh;
+        overflow: auto;
+    }
+    .no-scrollbar::-webkit-scrollbar {
+        display: none;
+    }
+    /* Hide scrollbar for IE, Edge and Firefox */
+    .no-scrollbar {
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
+    }
+</style>
+
+<div class="row d-flex overflow-auto flex-warp no-scrollbar" id="modules-container">
+</div>
+{{-- <div class="row overflow-scroll" style="height: 34vh;">
+    <div class="d-flex" id="modules-container">
     </div>
-</div
+</div> --}}
