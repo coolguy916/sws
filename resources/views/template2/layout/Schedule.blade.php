f79308aba43bd3aaf60061e42c9965e9fc909e48
lontongbalap <90811663+Mayfiq@users.noreply.github.com>
2024-06-18 21:07:39 +0700

    ketinggalan

---
+++ resources/views/template2/layout/Schedule.blade.php
@@ -0,0 +1,229 @@
+<!--
+=========================================================
+* Argon Dashboard 2 - v2.0.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('template2/assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('template2/assets/img/logos/logo.png') }}">
+    <title>
+        Swis
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('template2/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('template2/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{ asset('template2/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="{{ asset('template2/assets/css/argon-dashboard.css?v=2.0.4') }}" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
+        integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body class="g-sidenav-show bg-gray-100">
+
+    <div class="min-height-300 bg-primary position-absolute w-100"></div>
+
+    <!-- side navbar -->
+    <aside
+        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
+        id="sidenav-main">
+        <div class="sidenav-header">
+            <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+                aria-hidden="true" id="iconSidenav"></i>
+            <a class="navbar-brand m-0 d-flex justify-content-center"
+                href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
+                <img src="{{ asset('template2/assets/img/logos/logo2.png') }}" class="navbar-brand-img h-100"
+                    alt="main_logo">
+                <!-- <span class="ms-1 font-weight-bold">Swiss</span> -->
+            </a>
+
+
+        </div>
+
+        <hr class="horizontal dark mt-0">
+
+        <!-- Side bar -->
+        <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('schedule.dashboard') ? 'active' : '' }}"
+                        href="{{ route('schedule.dashboard') }}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('schedule.index') ? 'active' : '' }}"
+                        href="{{ route('schedule.index') }}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Schedule</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+    </aside>
+
+    <!-- Content -->
+    <main class="main-content position-relative border-radius-lg ">
+        <!-- Navbar -->
+        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
+            data-scroll="false">
+            <div class="container-fluid py-1 px-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white"
+                                href="/dashboard-user">Pages</a></li>
+                        <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
+                    </ol>
+                    <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+                </nav>
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+
+                        <div class="input-group">
+                            <!-- Jam -->
+                            <h5 class="text-white text-sm clock">Loading...</h5>
+                        </div>
+                    </div>
+
+                    <!-- User profile Button -->
+                    <ul class="navbar-nav justify-content-end">
+                        <li class="nav-item dropdown justify-content-end">
+                            <a class="nav-link text-white font-weight-bold px-0 dropdown-toggle"
+                                id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa fa-user me-sm-1"></i><span class="d-sm-inline d-none">{{Auth::user()->name}}</span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                                
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                      <i class="fa fa-power-off me-1 ms-1"></i>{{ __('Logout') }}</a>
+                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                              @csrf
+                                                          </form>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                            <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                                <div class="sidenav-toggler-inner">
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+
+
+                </div>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+
+        @yield('content')
+        @yield('tables')
+
+    </main>
+
+    <!-- Asset -->
+    <!--   Core JS Files   -->
+    <script src="https://kit.fontawesome.com/f7091fe396.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="{{ asset('template2/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('template2/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('template2/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('template2/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('template2/assets/js/plugins/chartjs.min.js') }}"></script>
+
+    @include('template2.js.js_chartdashboard')
+    @include('template2.js.js_dashboard')
+    @include('User.esp_control.jsmodalschedule')
+    @include('User.esp_control.jsstatisticmmodule')
+
+    <script>
+        function updateClock() {
+            const clockElement = document.querySelector('.clock');
+            const now = new Date();
+        
+            let hours = now.getHours();
+            let minutes = now.getMinutes();
+            let period = 'AM';
+        
+            if (hours >= 12) {
+                period = 'PM';
+                if (hours > 12) {
+                    hours -= 12;
+                }
+            } else if (hours === 0) {
+                hours = 12;
+            }
+        
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+        
+            const timeString = `${hours}:${minutes} ${period}`;
+            clockElement.textContent = timeString;
+        }
+        
+        setInterval(updateClock, 1000);
+        updateClock();  
+        </script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('template2/assets/js/argon-dashboard.min.js?v=2.0.4') }}"></script>
+</body>
+
+</html>
